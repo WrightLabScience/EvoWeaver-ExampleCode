7aa93d15e85d1b6e45a7e9e4ca9e05ba8ca9d3e8
Aidan Lakshman <ahl27@pitt.edu>
2023-10-09 20:08:53 +0100

    Add files via upload

---
+++ RunningProtWeaver/ExampleAnalysis.R
@@ -0,0 +1,22 @@
+library(SynExtend)
+
+# This contains two trees, two sets of location data, and a species tree
+load("ExampleData.RData")
+
+algos <- c("GainLoss", "Jaccard", "Hamming", "CorrGL", "MutualInformation", "PAPV",
+            "MirrorTree", "ContextTree", "TreeDistance",
+            "NVDT", "ResidueMI")
+colocAlgos <- c("Coloc", "ColocMoran", "TranscripMI")
+
+ew1 <- EvoWeaver(TwoComplexTrees, MySpeciesTree=ComplexSpecTree)
+ew2 <- EvoWeaver(TwoLocs, MySpeciesTree=ComplexSpecTree)
+
+predsNoColoc <- predict(ew1, Method=algos, TreeMethods='JRF', useDNA=FALSE, Processors=NULL)
+predsColoc <- predict(ew1, Method=colocAlgos, Processors=NULL)
+
+preds <- c(vapply(predsNoColoc, \(x) x[[2]], numeric(1L)),
+           vapply(predsColoc, \(x) x[[2]], numeric(1L)))
+
+names(preds) <- c(algos, colocAlgos)
+
+preds
